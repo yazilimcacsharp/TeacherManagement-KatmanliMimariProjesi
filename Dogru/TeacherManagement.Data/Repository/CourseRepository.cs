ae88ec13840fff4ca35496add8a0d38c7bd28954
yazilimcacsharp <yazilimca32@gmail.com>
2023-04-15 17:18:19 +0300

    bu branchde teacher course geliştirilmesi yapıldı
    bu branchde teacher course geliştirilmesi yapıldı

---
+++ Dogru/TeacherManagement.Data/Repository/CourseRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TeacherManagement.Data
+{
+    public class CourseRepository : ICourseRepository
+    {
+        Course ICourseRepository.GetCourseByCourseId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Course> ICourseRepository.GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+
+        Course ICourseRepository.InsertCourse(Course Course)
+        {
+            throw new NotImplementedException();
+        }
+
+        Course ICourseRepository.UpdateCourse(Course Course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
